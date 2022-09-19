e241cdb8d0c2952db0f393cc479de76a05fa3ead
Puchok-93 <54364298+Puchok-93@users.noreply.github.com>
2022-09-19 13:41:07 +0300

    Добавление начальной логики для фильтра

--- src/view/filter-view.js
+++ src/view/filter-view.js
@@ -1,24 +1,29 @@
 import AbstractView from '../framework/view/abstract-view.js';
 
 
-const createFilterTemplate = () => `
-<form class="trip-filters" action="#" method="get">
-<div class="trip-filters__filter">
-  <input id="filter-everything" class="trip-filters__filter-input  visually-hidden" type="radio" name="trip-filter" value="everything" checked>
-  <label class="trip-filters__filter-label" for="filter-everything">Everything</label>
-</div>
+const createFilterTemplate = function(points) {
+  return `<form class="trip-filters" action="#" method="get">
+    <div class="trip-filters__filter">
+      <input id="filter-everything" class="trip-filters__filter-input  visually-hidden" type="radio" name="trip-filter" value="everything" ${points.length === 0 ? 'disabled' : ''}>
+      <label class="trip-filters__filter-label" for="filter-everything">Everything</label>
+    </div>
+    <div class="trip-filters__filter">
+      <input id="filter-future" class="trip-filters__filter-input  visually-hidden" type="radio" name="trip-filter" value="future" ${points.length === 0 ? 'disabled' : ''}>
+      <label class="trip-filters__filter-label" for="filter-future">Future</label>
+    </div>
+   <button class="visually-hidden" type="submit">Accept filter</button>
+ </form>`;
+};
 
-<div class="trip-filters__filter">
-  <input id="filter-future" class="trip-filters__filter-input  visually-hidden" type="radio" name="trip-filter" value="future">
-  <label class="trip-filters__filter-label" for="filter-future">Future</label>
-</div>
+export default class FilterView extends AbstractView {
+  #points = null;
 
-<button class="visually-hidden" type="submit">Accept filter</button>
-</form>
-`;
+  constructor(points) {
+    super();
+    this.#points = points;
+  }
 
-export default class TripFilter extends AbstractView {
   get template() {
-    return createFilterTemplate();
+    return createFilterTemplate(this.#points);
   }
 }
